6437dd8e7a4fbb46fd745fd0a821592ffac840cc
红枫蓝叶 <3352941073@qq.com>
2026-01-03 20:14:29 +0800

    C语言标准库<string.h>部分函数的模拟实现
    C语言标准库<string.h>部分函数的模拟实现:包含memcpy,mommove,strcat,strcmp,strcpy,strlen,strncat,strncpy,strstr这几个函数

---
+++ C语言代码库string.h的模拟实现/C_2025_5_4_string.h.cpp
@@ -0,0 +1,14 @@
+﻿// C_2025_5_4_string.h.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//'strtok': This function or variable may be unsafe. Consider using strtok_s instead. To disable deprecation, use . See online help for details.
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[] = "192.168.1.1";
+	char str2[] = ".";
+	for (char* p = strtok(str, str2); p!=NULL; p = strtok(NULL, str2))
+		printf("%s\n", p);
+	return 0;
+}
---
+++ C语言代码库string.h的模拟实现/String.h
@@ -0,0 +1,145 @@
+﻿#include<assert.h>
+#include<stdlib.h>
+//strlen
+unsigned int strlen(const char* str)
+{
+	assert(str);
+	char* p = (char*)str;
+	while (*str)str++;
+	return str - p;
+}
+//strcpy
+
+char* strcpy(char* destination, const char* source)
+{
+	assert(destination&& source);
+	char* ret = destination;
+	while(*destination++ = *source++);
+	return ret;
+}
+//strncpy
+char* strncpy(
+	char* destination,
+	const char* source, 
+	int num
+	)
+	{
+		assert(destination && source);
+		char* ret = destination;
+		while (num--&& (*destination++ = *source++));
+		while (num--)*destination = '\0';
+		return ret;
+	}
+//strcat
+
+char* strcat(
+	char* destination,
+	const char* source
+	)
+	{
+	assert(destination && source);
+	char* ret = destination;
+	while (*destination)destination++;
+	while (*destination++ = *source++);
+	return ret;
+	}
+
+
+//strncat
+char* strncat(
+	char* destination, 
+	const char* source, 
+	int num
+	)
+{
+	assert(destination && source);
+	char* ret = destination;
+	while (*destination)destination++;
+	while (num--&&(*destination++ = *source++));
+	//*destination = '\0';
+	return ret;
+}
+//strcmp
+int strcmp(const char* str1, const char* str2)
+{
+	assert(str1 && str2);
+	while (*str1 == *str2)
+	{
+		if (*str2 == '\0')return 0;
+		str1++;
+		str2++;
+	}
+	return *str1 - *str2;
+}
+
+//strstr
+char* strstr(const char* p1, const char* p2)
+{
+	assert(p1 && p2);
+	while (true)
+	{
+		char* pp1 = (char*)p1;
+		char* pp2 = (char*)p2;
+		while (*pp1++ == *pp2++)if (!*pp2)return (char*)p1;
+		if (!*pp1)return NULL;
+		p1++;
+	}
+}
+
+//memcpy
+void* memcpy(void* destination,const void* source,unsigned long num)
+{
+	assert(destination && source);
+	char* p1 = (char*)destination;
+	char* p2 = (char*)source;
+	while (num--)*p1++ = *p2++;
+	return destination;
+}
+
+
+void* memmove_spcace(void* destination, void* source, unsigned long num)
+{
+	assert(destination && source);
+	char* p = (char*)malloc(num*sizeof(char));
+	assert(p);
+	char* rubbish = p;
+	char* pp = p;
+	char* p1 = (char*)destination;
+	char* ret = p1;
+	char* p2 = (char*)source;
+	int i = 0;
+	while (i++ != num)*p++ = *p2++;
+	i = 0;
+	while (i++ != num)*p1++ = *pp++;
+	free(rubbish);
+	return ret;
+}
+
+//情况一dest<source
+//    1 2 3 4 5 6 7 8 9
+//   /1 2 3 4 5/
+//		 /3 4 5 6 7/
+//情况二dest>source
+//    1 2 3 4 5 6 7 8 9
+//		 /3 4 5 6 7/
+//  / 1 2 3 4 5 /
+//没问题
+void* memmove_move(void* destination, void* source, unsigned long num)
+{
+	assert(destination && source);
+	char* p1 = (char*)destination;
+	char* p2 = (char*)source;
+	char* ret = p1;
+	if (destination > source)
+		while (num--)*(p1 + num) = *(p2 + num);
+	else
+	{
+		while (num--)*p1++ = *p2++;
+		return ret;
+	}
+}
+
+int memcpy(void* destination, void* source, unsigned long num)
+{
+	return 0;
+}
